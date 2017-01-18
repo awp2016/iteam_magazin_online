455a7559b37ecbe91e9730e6538d16d452113ec5
fulger <fulger@fulgers-MacBook-Pro.local>
2017-01-18 21:37:21 +0200

    Model and migration CustomUser, url for edit, and view edit user

--- src/iteam/urls.py
+++ src/iteam/urls.py
@@ -7,8 +7,7 @@ from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from django.views.decorators.cache import cache_page
 
-from . import views
-
+from iteam import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,6 +15,7 @@ urlpatterns = patterns('',
 	url(r'^shopping-cart/(?P<pk>\d+)/$', views.shopping_cart, name='shopping_cart'),
 	url(r'^remove-one-item/(?P<pk>\d+)/$', views.remove_item, name='remove_item'),
 	url(r'^add-item/(?P<pk_cart>\d+)/(?P<pk_produs>\d+)/(?P<quantity>\d+)/$', views.add_item, name='add_item'),
+    url(r'^edit_info/(?P<pk>\d+)/', views.EditProfileView.as_view(), name="edit_user"),
     url(r'^admin/', include(admin.site.urls)),  # NOQA
 )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
