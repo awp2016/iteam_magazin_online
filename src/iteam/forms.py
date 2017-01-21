f31fcd514f86aafd518dce1e69cb155c544f1c9a
Mihai Moiseanu <mihai.moiseanu@gmail.com>
2017-01-21 16:14:07 +0200

    Create search bar in layout the search is done by searching for substring in productName

--- src/iteam/forms.py
+++ src/iteam/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class CommentForm(forms.Form):
     text = forms.CharField(label="Comment", widget=forms.Textarea, required=True, max_length=500)
+
+
+class SearchForm(forms.Form):
+    text = forms.CharField(label="Search", widget=forms.TextInput, required=True, max_length=50)
