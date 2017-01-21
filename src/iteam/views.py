4b7193cd374ed746675bb94df1b9e2547f975d4e
Flaviu <flavium15@gmail.com>
2017-01-21 17:31:21 +0200

    Fixed shopping history

--- src/iteam/views.py
+++ src/iteam/views.py
@@ -127,10 +127,15 @@ def remove_item(request, pk):
         order.save()
 
     orders = Order.objects.filter(cart=cart)
+    tup = []
+    for ord in orders:
+        image = models.Image.objects.filter(product=ord.product).first
+        tuplu = (ord, image)
+        tup.append(tuplu)
     context = {
         'cart': cart,
         'user': user,
-        'orders': orders
+        'tuplu': tup
     }
     return render(request, 'view_shopping_cart.html', context)
 
@@ -139,7 +144,7 @@ def add_item(request, pk_cart, pk_produs):
     cart = ShoppingCart.objects.get(pk=pk_cart)
     product = Product.objects.get(pk=pk_produs)
     try:
-        check_order = Order.objects.get(product=product)
+        check_order = Order.objects.get(product=product,cart=cart)
     except Exception:
         order = Order(cart=cart, product=product, quantity=1)
         order.save()
